b4782e9962a60434c1cff1ae856e72e2df4a6574
斯杰 <devhacker520@hotmail.com>
2013-12-29 19:31:41 +0800

    增加session、对象封装、post处理等-测试

--- start.js
+++ start.js
@@ -1,5 +1,9 @@
 var path = require('path');
 var harbors = require('harbors2');
 
+harbors.config(path.join(__dirname,'./config/server.ini'));
+harbors.config(path.join(__dirname,'./config/vhost.ini'));
+
+harbors.module('mysql',require('./module/mysql'));
 
 harbors.start();
