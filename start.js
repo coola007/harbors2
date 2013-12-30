14aee9ac0758c229b7e09fe574b5b1803da460eb
斯杰 <devhacker520@hotmail.com>
2013-12-30 23:29:55 +0800

    注释代码，重写config.load

--- start.js
+++ start.js
@@ -1,9 +1,9 @@
 var path = require('path');
 var harbors = require('harbors2');
 
-harbors.config(path.join(__dirname,'./config/server.ini'));
-harbors.config(path.join(__dirname,'./config/vhost.ini'));
-
-harbors.module('mysql',require('./module/mysql'));
+//harbors.config(path.join(__dirname,'./config/server.ini'));
+//harbors.config(path.join(__dirname,'./config/vhost.ini'));
+//
+//harbors.module('mysql',require('./module/mysql'));
 
 harbors.start();
