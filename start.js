c9db93e615c8b3daa4eef82f1d097748776a60f6
斯杰 <devhacker520@hotmail.com>
2014-01-02 22:48:16 +0800

    提取方法，优化封装过程，减少不必要的运算

--- start.js
+++ start.js
@@ -4,18 +4,18 @@ var harbors = require('harbors2');
 harbors.config(path.join(__dirname,'./config/server.ini'));
 harbors.config(path.join(__dirname,'./config/vhost.ini'));
 
-harbors.module('mysql',require('./module/mysql'));//自定义外部模块
+//harbors.module('mysql',require('./module/mysql'));//自定义外部模块
 
-harbors.rewrite('index',function(req){//自定义一个名为index的重写模块（可在配置中引用）
-    var _sp = req.url.split('?');
-    var _get = querystring.parse(_sp[1]);
-    return {
-        Module:'index_.js',
-        Extname:'',
-        Url:'/',
-        File:'',
-        Get:_get
-    };
-});
+//harbors.rewrite('index',function(req){//自定义一个名为index的重写模块（可在配置中引用）
+//    var _sp = req.url.split('?');
+//    var _get = querystring.parse(_sp[1]);
+//    return {
+//        Module:'index_.js',
+//        Extname:'',
+//        Url:'/',
+//        File:'',
+//        Get:_get
+//    };
+//});
 
 harbors.start();
